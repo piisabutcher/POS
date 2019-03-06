43b0c91b4dd83eb8d550bff95d1d10a716f1614b
Administrator <Administrator@USER-20181223FD>
2019-03-06 20:06:42 +0800

    test demo

---
+++ sb Maven Webapp/src/main/java/test/TestApplication.java
@@ -0,0 +1,21 @@
+package test;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+@Controller
+@SpringBootApplication
+
+public class TestApplication {
+	@RequestMapping("hello")
+    @ResponseBody
+    public String hello(){
+        return "hello world！";
+    }
+    
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+
+
+}
