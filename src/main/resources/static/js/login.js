d6dd06857f31b34688b16892654e1b75f9eed58f
piisabutcher <627534595@qq.com>
2019-03-09 12:49:23 +0800

    实现登录功能和页面

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
     console.log("success");
 })
+
+$("#login>form>input[type=button]").click(function(event) {
+	console.log("尝试登录……");
+	$.ajax({
+		url: '/user/getAllUser',
+		type: 'post',
+		dataType: 'json',
+		data: {
+			userId: $("#userId").val(),
+			password: $("password").val()
+		},
+		success:function(res){
+			location.pathname = "pages/index.html";
+		},
+		error:function(){
+		    alert("出错啦！");
+
+
+        }
+	});
+	
+	
+});
